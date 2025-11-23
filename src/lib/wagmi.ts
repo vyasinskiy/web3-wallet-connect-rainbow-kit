1ac63597e5fecf32d1e8216b9aabd21a4c62c141
vyasinsky <yasinsky93@gmail.com>
2025-11-23 11:47:35 +0100

    Expand wagmi config

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,18 +1,19 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { http } from "wagmi";
-import { mainnet, arbitrum } from "wagmi/chains";
+import { mainnet, arbitrum, optimism, base, bsc } from "wagmi/chains";
 
-const projectId =
-  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ||
-  "00000000000000000000000000000000";
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
 
 export const wagmiConfig = getDefaultConfig({
   appName: "Web3 Wallet Connect",
-  projectId,
-  chains: [mainnet, arbitrum], // important to allow rainbow show the list of chains
+  projectId, // required property from wallet connect cloud
+  chains: [mainnet, arbitrum, optimism, base, bsc], // important to allow rainbow show the list of chains
   transports: {
     [mainnet.id]: http(),
     [arbitrum.id]: http(),
+    [optimism.id]: http(),
+    [base.id]: http(),
+    [bsc.id]: http(),
   },
   ssr: true,
 });
